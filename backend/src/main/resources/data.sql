be016e47e51147ea07cc91bfefa1e471a2d2bbe5
Eliezer Moraes Silva <eliezer.silva@spread.com.br>
2022-03-11 11:35:43 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,4 +13,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Curso Java' , 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 'https://cdn3.vectorstock.com/i/1000x1000/58/27/course-vector-28055827.jpg');
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Curso Java', 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 'https://cdn3.vectorstock.com/i/1000x1000/58/27/course-vector-28055827.jpg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00', 1);
