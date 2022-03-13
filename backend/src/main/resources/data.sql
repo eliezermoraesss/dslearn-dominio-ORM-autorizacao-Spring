bc79176f1a73fd4f2d93370221495a26b5cf49a5
Eliezer Moraes Silva <eliezer.silva@spread.com.br>
2022-03-13 06:34:06 -0300

    Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,11 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Curso Java
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00', 1);
+
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 1, 1);
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 2, 1);
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 0, 1);
+
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar!', 1, 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 1, null);
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos começar continuar!', 2, 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 1, 1);
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos começar finalizar!', 3, 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 1, 2);
