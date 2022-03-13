108b6bbc3d2ffe7c67dd1ebed0fd1683a83265b6
Eliezer Moraes Silva <eliezer.silva@spread.com.br>
2022-03-13 08:01:21 -0300

    Enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -25,3 +25,6 @@ INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) V
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar!', 1, 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 1, null);
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos começar continuar!', 2, 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 1, 1);
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos começar finalizar!', 3, 'https://cdn4.vectorstock.com/i/1000x1000/65/28/online-training-course-and-distance-learning-vector-31326528.jpg', 1, 2);
+
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00', null, true, false);
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00', null, true, false);
