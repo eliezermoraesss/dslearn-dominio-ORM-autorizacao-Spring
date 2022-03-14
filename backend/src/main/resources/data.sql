d10c1994693900e9726fa8515faa8f233cfabce1
Eliezer Moraes Silva <eliezer.silva@spread.com.br>
2022-03-14 13:52:53 -0300

    Lesson seed, fix: lessonsDone

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -28,3 +28,18 @@ INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prere
 
 INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00', null, true, false);
 INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00', null, true, false);
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 1 do capítulo 1', 1, 1);
+INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (1, 'Material de apoio: abc', 'https://www.youtube.com/watch?v=xVaRrdAs5NI');
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 2 do capítulo 1', 2, 1);
+INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (2, 'Material de apoio: jjjjj', 'https://www.youtube.com/watch?v=xVaRrdAs5NI');
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 3 do capítulo 1', 3, 1);
+INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (3, 'kkkkkkkkk', 'https://www.youtube.com/watch?v=xVaRrdAs5NI');
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Tarefa do capítulo 1', 4, 1);
+INSERT INTO tb_task(id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-12-25T03:00:00');
+
+INSERT INTO tb_lessons_done(lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done(lesson_id, user_id, offer_id) VALUES (2, 1, 1);
