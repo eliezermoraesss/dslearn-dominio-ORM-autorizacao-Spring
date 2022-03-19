595de0e89f076d97a69f3b7c15d35e8e14eefbb4
Eliezer Moraes Silva <eliezer.silva@spread.com.br>
2022-03-19 15:34:48 -0300

    Repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/EnrollmentRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dslearnbds.entities.Enrollment;
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentPK> {
+
+}
